ccf2f0a751fea9964990849ce51746f9794036de
Leonardo Daudinot <ldaudinot10@gmail.com>
2018-06-22 23:05:55 -0400

    My MVC refactoring

--- js/script.js
+++ js/script.js
@@ -2,61 +2,92 @@
 
 (() => {
   const cats = [{
-    name: 'miau',
-    URL: 'https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496',
-    counter: 0
-  },
-  {
-    name: 'meow',
-    URL: 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
-    counter: 3
-  },
-  {
-    name: 'Peter',
-    URL: 'https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454',
-    counter: 0
-  },
-  {
-    name: 'Blackish',
-    URL: 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
-    counter: 0
-  },
-  {
-    name: 'Leon',
-    URL: 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
-    counter: 0
-  }
+      name: 'miau',
+      URL: 'https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496',
+      counter: 0
+    },
+    {
+      name: 'meow',
+      URL: 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
+      counter: 3
+    },
+    {
+      name: 'Peter',
+      URL: 'https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454',
+      counter: 0
+    },
+    {
+      name: 'Blackish',
+      URL: 'https://lh4.ggpht.com/dUJNejPqb_qLsV1kfWcvviqc7adxsw02BSAm8YLWNklP4lI6fQCLKXd-28uKuchtjoEUpqFN0K6kkTSDHw=s0#w=588&h=640',
+      counter: 0
+    },
+    {
+      name: 'Leon',
+      URL: 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
+      counter: 0
+    }
   ];
 
-  let activeCat = cats[0];
+  const octopus = {
+    activeCat: null,
 
-  const list = document.querySelector('#list-cats');
-  const containerCat = document.querySelector('.content-cat');
-  const paraName = document.querySelector('.cat-name');
-  const imgCat = document.querySelector('.img-cat');
-  const spanCountCat = document.querySelector('#count-cat');
+    init: () => {
+      octopus.activeCat = cats[0];
+      viewMenu.init();
+      viewCat.init();
+    },
 
-  cats.forEach((cat) => {
-    const li = document.createElement('li');
-    li.textContent = cat.name;
-    li.addEventListener('click', () => {
-      activeCat = cat;
-      showCat( /*paraName, cat, imgCat, spanCountCat*/ );
-    });
-    list.appendChild(li);
-  });
+    getActiveCat: () => octopus.activeCat,
 
-  containerCat.addEventListener('click', () => {
-    const temp = ++activeCat.counter;
-    spanCountCat.textContent = temp;
-  });
+    setActiveCat: (cat) => octopus.activeCat = cat,
 
-  function showCat( /*paraName, cat, imgCat, spanCountCat*/ ) {
-    paraName.textContent = activeCat.name;
-    imgCat.setAttribute('src', activeCat.URL);
-    imgCat.setAttribute('alt', `Cat ${activeCat.name}`);
-    spanCountCat.textContent = activeCat.counter;
-  }
+    incClickCounter: () => ++octopus.activeCat.counter
+  };
 
-  showCat();
+  const viewMenu = {
+
+    list: document.querySelector('#list-cats'),
+
+    init: () => {
+      viewMenu.renderMenu();
+    },
+
+    renderMenu: () => {
+      cats.forEach((cat) => {
+        const li = document.createElement('li');
+        const btn = document.createElement('button');
+        btn.textContent = cat.name;
+        li.appendChild(btn);
+        btn.addEventListener('click', () => {
+          octopus.setActiveCat(cat);
+          viewCat.renderCat();
+        });
+        viewMenu.list.appendChild(li);
+      });
+    }
+
+  };
+
+  const viewCat = {
+
+    paraName: document.querySelector('.cat-name'),
+    imgCat: document.querySelector('.img-cat'),
+    spanCountCat: document.querySelector('#count-cat'),
+
+    init: () => {
+      const containerCat = document.querySelector('.content-cat');
+      containerCat.addEventListener('click', () => viewCat.spanCountCat.textContent = octopus.incClickCounter());
+      viewCat.renderCat();
+    },
+
+    renderCat: () => {
+      viewCat.paraName.textContent = octopus.getActiveCat().name;
+      viewCat.imgCat.setAttribute('src', octopus.getActiveCat().URL);
+      viewCat.imgCat.setAttribute('alt', `Cat ${octopus.getActiveCat().name}`);
+      viewCat.spanCountCat.textContent = octopus.getActiveCat().counter;
+    }
+  };
+
+  octopus.init();
+
 })();
